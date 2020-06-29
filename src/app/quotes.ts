9d8afb443c63519871c7b29b71af8a7418909077
Wigwa <lucywigwa7@gmail.com>
2020-06-29 17:42:44 +0300

    add dislike and like button

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,7 @@
 export class Quotes {
     showDescription:boolean;
     constructor(public id: number,
+        public dislikes:number, public likes:number,
    public name: string, public title:string) {
        this.showDescription=false;
    }
